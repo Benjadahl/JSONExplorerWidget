4f4c387b97cf4ba1b6c1f016d9b3589c2e2e3814
Benjadahl <benjadahl@gmail.com>
2017-02-05 20:22:35 +0100

    Expanded the test object

--- demo.js
+++ demo.js
@@ -1,7 +1,17 @@
 var app = new Vue({
   el: "#vueApp",
   data: {
-    "JSONobject": {name: "Bob", lastName: "Builder", hat: ["Yellow", "Protect"], age: 25},
+    "JSONobject": {
+      name: "Bob",
+      lastName: "Builder",
+      hat: ["Yellow", "Protect"],
+      age: 25,
+      child: {
+        name: "ChildName",
+        lastName: "ChildLastName",
+        age: 5
+      }
+    },
     "result": ""
   },
   methods: {
